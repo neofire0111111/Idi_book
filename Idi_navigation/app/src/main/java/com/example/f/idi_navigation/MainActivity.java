2b0edb2c79130acf1331c078fa0892f214c0c9a3
neofire0111111 <neofire.0111111@gmail.com>
2016-11-30 11:45:56 +0100

    a

---
+++ Idi_navigation/app/src/main/java/com/example/f/idi_navigation/MainActivity.java
@@ -0,0 +1,212 @@
+package com.example.f.idi_navigation;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private MySQLiteHelper db;
+
+    private ListView list;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> alist;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        db = new MySQLiteHelper(this); //object database
+
+
+
+
+
+
+        //*-Button per drawer manager-*//
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        //*-Button per drawer manager-*//
+
+
+
+
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+
+        BookData bookData = new BookData(this);
+        bookData.open();
+
+        List<Book> values = bookData.getAllBooks();
+
+        // use the SimpleCursorAdapter to show the
+        // elements in a ListView
+        ArrayAdapter<Book> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+
+    }
+
+    private void setListAdapter(ArrayAdapter<Book> adapter) {
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+
+        // Handle the camera action
+        if (id == R.id.nav_category) {
+
+            Book b = new Book();
+            b.setTitle("harry potter");
+            b.setAuthor("rowling");
+            b.setPublisher("a");
+            b.setCategory("aaa");
+
+           // db.addBook(b);
+
+            ListCategoryFragment pf = new ListCategoryFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.relativeLayour_fragment, pf, pf.getTag()).commit();
+
+        } else if (id == R.id.search_book) {
+
+
+            Context context = getApplicationContext();
+
+            int duration = Toast.LENGTH_SHORT;
+
+           // Toast toast = Toast.makeText(context, ((String.valueOf(db.getSize())) ), duration);
+           // toast.show();
+
+
+
+            SearchFragment sf = new SearchFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.relativeLayour_fragment, sf, sf.getTag()).commit();
+
+        } else if (id == R.id.nav_add) {
+
+            AddFragment sf = new AddFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.relativeLayour_fragment, sf, sf.getTag()).commit();
+
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
---
+++ Idi_navigation/app/src/main/java/com/example/f/idi_navigation/MySQLiteHelper.java
@@ -0,0 +1,146 @@
+package com.example.f.idi_navigation;
+
+/**
+ * MySQLiteHelper
+ * Created by pr_idi on 10/11/16.
+ */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.List;
+import java.util.LinkedList;
+
+
+public class MySQLiteHelper extends SQLiteOpenHelper {
+
+
+
+
+    public static final String TABLE_BOOKS = "books";
+    public static final String COLUMN_ID = "_id";
+
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_AUTHOR = "author";
+    public static final String COLUMN_YEAR = "year";
+    public static final String COLUMN_PUBLISHER= "publisher";
+    public static final String COLUMN_CATEGORY= "category";
+    public static final String COLUMN_PERSONAL_EVALUATION = "personal_evaluation";
+
+
+    private static final String DATABASE_NAME = "books.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Database creation sql statement
+    private static final String DATABASE_CREATE = "create table " + TABLE_BOOKS + "( "
+            + COLUMN_ID + " integer primary key autoincrement, "
+            + COLUMN_TITLE + " text not null, "
+            + COLUMN_AUTHOR + " text not null, "
+            + COLUMN_YEAR + " integer, "
+            + COLUMN_PUBLISHER + " text not null, "
+            + COLUMN_CATEGORY + " text not null, "
+            + COLUMN_PERSONAL_EVALUATION + " text"
+            + ");";
+
+    public MySQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+
+    }
+
+ /*   public int getSize() {
+
+            int i=0;
+        // 1. build the query
+        String query = "SELECT  * FROM " + TABLE_BOOKS;
+
+        // 2. get reference to writable DB
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        // 3. go over each row, build book and add it to list
+
+        if (cursor.moveToFirst()) {
+            do {i++;
+            } while (cursor.moveToNext());
+        }
+
+        return i;
+    }*/
+   /* public List<Book> getAllBooks() {
+        List <Book> books = new LinkedList<Book>() ;
+
+        // 1. build the query
+        String query = "SELECT  * FROM " + TABLE_BOOKS;
+
+        // 2. get reference to writable DB
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        // 3. go over each row, build book and add it to list
+        Book book = null;
+        if (cursor.moveToFirst()) {
+            do {
+                book = new Book();
+                book.setId(Integer.parseInt(cursor.getString(0)));
+                book.setTitle(cursor.getString(1));
+                book.setAuthor(cursor.getString(2));
+
+                // Add book to books
+                books.add(book);
+            } while (cursor.moveToNext());
+        }
+
+        Log.d("getAllBooks()", books.toString());
+
+        // return books
+        return books;
+    }*/
+
+
+   /* public void addBook(Book book){
+        Log.d("addBook", book.toString());
+        // 1. get reference to writable DB
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // 2. create ContentValues to add key "column"/value
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_TITLE, book.getTitle()); // get title
+        values.put(COLUMN_AUTHOR, book.getAuthor()); // get author
+        values.put(COLUMN_PUBLISHER, book.getPublisher());
+        values.put(COLUMN_CATEGORY, book.getCategory());
+        // 3. insert
+        db.insert(TABLE_BOOKS, // table
+                null, //nullColumnHack
+                values); // key/value -> keys = column names/ values = column values
+
+        // 4. close
+        db.close();
+
+    }*/
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(MySQLiteHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_BOOKS);
+        onCreate(db);
+    }
+
+}
